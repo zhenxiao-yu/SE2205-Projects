a8311ee97332bcf0d2acd32d3e82286d8aaa2901
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-04-15 20:28:53 -0400

    Update Comp.java

--- Lab7/Comp.java
+++ Lab7/Comp.java
@@ -1,1 +1,8 @@
+import java.util.Comparator;
 
+public class Comp<E> implements Comparator<E> {
+    public int compare(E data1, E data2) {
+        return ((Comparable<E>) data1).compareTo(data2);
+    }
+
+}
